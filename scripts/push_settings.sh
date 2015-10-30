9b8eb6ae3cca483f259475c4b3b7f9124035e70c
Eric Trudeau <erictrudeau@me.com>
2015-10-30 10:15:57 -0400

    update

---
+++ scripts/push_settings.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+scp ~/Projects/matrix_collector_linux/config/settings_brocade.py stmtx:/opt/matrix/matrix_collector_linux/config/
+scp ~/Projects/matrix_collector_linux/config/settings_emc_vnx_block.py stmtx:/opt/matrix/matrix_collector_linux/config/
+scp ~/Projects/matrix_collector_linux/config/settings_emc_vnx_file.py stmtx:/opt/matrix/matrix_collector_linux/config/
+scp ~/Projects/matrix_collector_linux/config/settings_mitrend.py stmtx:/opt/matrix/matrix_collector_linux/config/
+scp ~/Projects/matrix_collector_linux/config/settings_secure.py stmtx:/opt/matrix/matrix_collector_linux/config/
